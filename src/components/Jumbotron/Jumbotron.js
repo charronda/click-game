07bbe8b54f6658ab7ddfc20d8f9ba1f8bbfe57ee
charronda <cking947@gmail.com>
2018-05-10 20:25:48 -0400

    udate styles

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css";
 // Jumbotron component
 const Jumbotron = props => (
   <div className="jumbotron jumbotron-fluid bg-cover text-white text-center">
-    <h1 className="display-4 font-weight-bold">Welcome to the Wakanda Clicky Game!</h1>
+    <h1 className="display-4 font-weight-bold">Coffee Matching with Love!</h1>
     <p className="lead font-weight-bold">Click on an image to earn points, but dont click the same image twice!</p>
   </div>
 );
